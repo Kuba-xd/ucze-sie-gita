e042fbae94de4435dad2a4851cc694d21feb7938
Kuba-xd <97913832+Kuba-xd@users.noreply.github.com>
2022-03-06 13:16:52 +0100

    Update nowy_plik.py

--- nowy_plik.py
+++ nowy_plik.py
@@ -1,1 +1,27 @@
-print(x)
+x = ['a', 'ab', 'abc', 'abcd']
+
+print(x, type(x))
+
+for e in x:
+    if 'b' in e:
+        print(e)
+
+min = min(x)
+print(min)
+
+max = max(x)
+print(len(max))
+
+print(x[1])
+print()
+
+def efekt(x):
+    najkrotsza = x[0]
+    for e in x:
+        if len(e) < len(najkrotsza):
+            najkrotsza = e
+    return najkrotsza 
+
+najkrotsza = efekt(x)
+print(najkrotsza)
+
